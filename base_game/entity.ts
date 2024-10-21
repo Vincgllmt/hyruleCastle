11e4bedf72a471334e7fbade3b807dbc3adfa0e1
guille_b <guille_b@etna-alternance.net>
2024-10-21 11:20:31 +0000

    fix: :bug: Correction de rarity

--- base_game/entity.ts
+++ base_game/entity.ts
@@ -14,42 +14,40 @@ export default interface Entity {
 function getRarityAndArray(data: Entity[]) {
     let dataEntity: number[] = []
     const random: number = Math.floor(Math.random() * (100 - 1 + 1) + 1)
-    let rarity = 0
-    switch (random <= 100) {
-        case random <= 50: {
-            rarity = 1
-        }
-        case random <= 80 && random >= 51: {
-            rarity = 2
-        }
-        case random >= 81 && random <= 95: {
-            rarity = 3
-        }
-        case random >= 96 && random <= 99: {
-            rarity = 4
-        }
-        default: {
-            rarity = 5
-        }
-        for (let i = 0; i < data.length; i += 1) {
-            if (data[i].rarity === rarity) {
-                dataEntity.push(data[i].id)
-            }
+    let rarity = 1
+    if (random >= 51 && random <= 80) {
+        rarity = 2
+    }
+    else if (random >= 81 && random <= 95) {
+        rarity = 3
+    }
+    else if (random >= 96 && random <= 99) {
+        rarity = 4
+    }
+    else if (random === 100) {
+        rarity = 5
+    }
+    for (let i = 0; i < data.length; i += 1) {
+        if (data[i].rarity === rarity) {
+            dataEntity.push(data[i].id)
         }
     }
-    return dataEntity
+    if (dataEntity.length === 1) {
+        return dataEntity[0]
+    }
+    else {
+        return dataEntity[Math.floor(Math.random() * dataEntity.length)]
+    }
 }
 export function getRandomEntity(path: string) {
     const data: Entity[] = readJSON(path);
     let dataId = getRarityAndArray(readJSON(path));
-    let entity: Entity = {name: "", id: 1, hp: 1, maxhp: 1, str: 1, rarity: 1}
-    const entityId = dataId[Math.floor(Math.random() * data.length)];
+    let entity: Entity = { name: "", id: 1, hp: 1, maxhp: 1, str: 1, rarity: 1 }
     for (let i = 0; i < data.length; i += 1) {
-        if (data[i].id === entityId) {
+        if (data[i].id === dataId) {
             entity = data[i]
         }
     }
     return entity
-    
+
 }
-console.log(getRandomEntity('resources/bosses.json'))
