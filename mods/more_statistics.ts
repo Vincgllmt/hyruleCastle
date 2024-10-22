d1706cfc07d8d65df6a61266f5a33e625508db84
guille_b <guille_b@etna-alternance.net>
2024-10-22 14:23:47 +0000

    feat: :sparkles: Ajout force et faiblesse pour les skills (qui ont des damage)

--- mods/more_statistics.ts
+++ mods/more_statistics.ts
@@ -70,14 +70,100 @@ function attack(caster: Entity, target: Entity) {
         console.log(`${target.name} dodged !`)
     }
 }
-function skill(caster: Entity, target: Entity) {}
+function skills(caster: Entity, target: Entity, spell: Skill) {
+    const weakRace = getWeakness(caster, 1)
+    const strengthRace = getStrongest(caster, 1)
+    const weakClass = getWeakness(caster, 2)
+    const strengthClass = getStrongest(caster, 2)
+    let weak = false;
+    let strong = false;
+    let luck = false;
+    const dodge = caster.spd - target.spd
+    let dodgePossible = false;
+    const randomDodge = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+    const randomCrit = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+    let spellAtk = spell.dmg
+    if (spell !== undefined && spell.dmg > 0) {
+        // weakRace
+        if (weakRace.includes(target.race)) {
+            spellAtk = spellAtk / 2
+            weak = true
+        }
+        // weakClass
+        if (weakClass.includes(target.class)) {
+            spellAtk = spellAtk / 2
+            weak = true
+        }
+        // strengthRace
+        if (strengthRace.includes(target.race)) {
+            spellAtk = spellAtk * 2
+            strong = true
+        }
+        // strengthRace
+        if (strengthClass.includes(target.class)) {
+            spellAtk = spellAtk * 2
+            strong = true
+        }
+        if (randomCrit <= target.luck) {
+            luck = true
+            spellAtk = spellAtk * 2
+        }
+        if (randomDodge <= dodge) {
+            dodgePossible = true
+        }
+        if (dodgePossible !== true) {
+            if (luck) {
+                if (weak && !strong) {
+                    console.log(`${caster.name} make a glancing hit.. But with a crit !`)
+                }
+                if (!weak && strong) {
+                    console.log(`${caster.name} make a crushing hit and a crit !`)
+                }
+                console.log(`CRITICAL HIT | ${caster.name} attack the ${target.name} ! Dealing ${spellAtk - target.res} of damage !! `)
+                target.hp -= spellAtk - target.res
+            }
+            else {
+                if (weak && !strong) {
+                    console.log(`${caster.name} make a glancing hit..`)
+                }
+                if (!weak && strong) {
+                    console.log(`${caster.name} make a crushing hit !`)
+                }
+                console.log(`${caster.name} attack the ${target.name} ! Dealing ${spellAtk - target.res} of damage.`)
+                target.hp -= spellAtk - target.res
+            }
+        }
+        else {
+            console.log(`${target.name} dodged !`)
+        }
+    }
+    else if (spell !== undefined && spell.dmg !== 0) {
+        const splitted = spell.effect.split('_')
+        if (splitted[0].toLowerCase() === 'heal') {
+            caster.hp += +splitted[1]
+            const diff = caster.hp - caster.maxhp
+            if (diff > 0) {
+                caster.hp -= diff
+            }
+            console.log(`You healed of ${splitted[1]} hp !`)
+        }
+        else {
+            caster.mp += +splitted[1]
+            const diff = caster.mp - caster.maxmp
+            if (diff > 0) {
+                caster.mp -= diff
+            }
+            console.log(`You restored of ${splitted[1]} mp !`)
+        }
+    }
+}
 function handleTurnAttack(playerFirstTurn: boolean, enemy: Entity, player: Entity, difficulty: number) {
     enemy.str = Math.floor(enemy.str * difficulty)
     if (playerFirstTurn) {
         attack(player, enemy);
         if (enemy.hp > 0) {
             attack(enemy, player);
-        } 
+        }
     }
     else {
         attack(enemy, player);
@@ -90,50 +176,20 @@ function handleTurnSkills(player: Entity, enemy: Entity, spells: Skill[], diffic
     const enemyAtk = Math.floor(enemy.str * difficulty)
     const playerFirstTurn = enemy.spd < player.spd
     const skill = showSkills(spells, player);
-    if (skill !== undefined && skill.dmg > 0) {
+    if (skill !== undefined) {
         if (playerFirstTurn) {
-            console.log(`Used ${skill.name} !\ndealed ${skill.dmg - enemy.res} damage !`)
-            enemy.hp -= skill.dmg - enemy.res
+            skills(player,enemy,skill)
             if (enemy.hp > 0) {
-                console.log(`the ${enemy.name} attack ! You lost ${enemyAtk - player.def} hp !`)
-                player.hp -= enemyAtk - player.def
-                player.mp -= skill.cost
+                attack(enemy, player)
             }
         }
         else {
-            console.log(`the ${enemy.name} attack ! You lost ${enemyAtk - player.def} hp !`)
-            player.hp -= enemyAtk - player.def
+            attack(enemy, player)
             if (player.hp > 0) {
-                console.log(`Used ${skill.name} !\ndealed ${skill.dmg - enemy.res} damage !`)
-                enemy.hp -= skill.dmg - enemy.res
-                player.mp -= skill.cost
+                skills(player, enemy, skill)
             }
-        }
-    } 
-    else if (skill !== undefined && skill.dmg !== 0) {
-        const splitted = skill.effect.split('_')
-        if (splitted[0].toLowerCase() === 'heal') {
-            player.hp += +splitted[1]
-            const diff = player.hp - player.maxhp
-            if (diff > 0) {
-                player.hp -= diff
-            }
-            console.log(`You healed of ${splitted[1]} hp !`)
-            console.log(`the ${enemy.name} attack ! You lost ${enemyAtk - player.def} hp !`)
-            player.hp -= enemyAtk - player.def
-        }
-        else {
-            player.mp += +splitted[1]
-            const diff = player.mp - player.maxmp
-            if (diff > 0) {
-                player.mp -= diff
-            }
-            console.log(`You restored of ${splitted[1]} mp !`)
-            console.log(`the ${enemy.name} attack ! You lost ${enemyAtk - player.def} hp !`)
-            player.hp -= enemyAtk - player.def
         }
     }
-
 }
 export default function handleTurn(response: string, enemy: Entity, player: Entity, difficulty: number) {
     const enemyAtk = Math.floor(enemy.str * difficulty)
